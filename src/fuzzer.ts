672168a1c843708a3fc08ccfc058a1cbbb319b8e
Alex Van Camp <vancamp.alex@gmail.com>
2019-07-27 19:29:36 -0500

    feat: add basic random bytes fuzzer (#2)

---
+++ src/fuzzer.ts
@@ -0,0 +1,50 @@
+// Native
+import {randomBytes} from 'crypto';
+
+// Packages
+import * as osc from 'osc';
+import {randomUniform} from 'd3-random';
+
+// Ours
+import {createLogger} from './logging';
+import {createSocket} from './socket';
+
+const FUZZ_INTERVAL = 10;
+
+export async function createFuzzer() {
+	const oscSocket = await createSocket();
+	return new Fuzzer(oscSocket);
+}
+
+class Fuzzer {
+	private _oscSocket: osc.UDPPort;
+
+	private _log = createLogger('fuzzer');
+
+	private _fuzzInterval?: NodeJS.Timeout;
+
+	constructor(oscSocket: osc.UDPPort) {
+		this._oscSocket = oscSocket;
+	}
+
+	start() {
+		this.stop();
+		this._log.info('Starting.');
+		this._fuzzInterval = setInterval(() => {
+			this.sendRealGarbage();
+		}, FUZZ_INTERVAL);
+	}
+
+	stop() {
+		this._log.info('Stopping');
+		clearInterval(this._fuzzInterval!);
+	}
+
+	sendRealGarbage() {
+		const numBytes = Math.round(randomUniform(1, 512)());
+		this._oscSocket.socket.send(
+			randomBytes(numBytes),
+			this._oscSocket.options.remotePort!,
+		);
+	}
+}
--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,6 @@
 // Packages
 import * as osc from 'osc';
+import * as getPort from 'get-port';
 
 // Ours
 import {createLogger} from './logging';
@@ -8,12 +9,13 @@ const log = createLogger('socket');
 const X32_UDP_PORT = 10023;
 
 export async function createSocket() {
+	const localPort = await getPort();
 	return new Promise<osc.UDPPort>((resolve, reject) => {
 		let settled = false;
 
 		const oscSocket = new osc.UDPPort({
 			localAddress: '0.0.0.0',
-			localPort: 52361,
+			localPort,
 			remoteAddress: '192.168.1.9',
 			remotePort: X32_UDP_PORT,
 			metadata: true,
